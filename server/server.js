37f920107e3543be0e0b53a2df100e8383687f10
Renan <renan.baialuna@gmail.com>
2018-06-10 21:05:15 -0300

    Initial Comit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+
+const express = require('express');
+
+var app = express();
+app.use(express.static(publicPath))
+app.listen(port,() =>{
+    console.log(`server is up on ${port}`)
+})
+
+
+console.log(__dirname + '/../public');
+console.log(publicPath);
