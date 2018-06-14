34bf0c0b63b31f493416daef8b91565f7e695b51
Renan <renan.baialuna@gmail.com>
2018-06-13 22:54:29 -0300

    Add geolocation support via geolocation api

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,17 @@
-var generateMessage = (from, text) =>{
+var generateMessage = (from, text) => {
     return {
         from,
         text,
         createdAt: new Date().getTime()
     }
 }
-module.exports = {generateMessage}
+
+var generateLocationMessage = (from, latitude, longitude) => {
+    return {
+        from,
+        url: `http://www.google.com/maps?q=${latitude},${longitude}`,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = { generateMessage, generateLocationMessage }
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () =>{
     it('should generate correct message object', () =>{
@@ -14,8 +14,21 @@ describe('generate message', () =>{
         })
         expect(message.from).toBe(from)
         expect(message.text).toBe(text)
-
-
-
     });
+})
+
+describe('generateLocationMessage', () =>{
+    it('should generate correct location object', ()=>{
+        var from= 'jjj'
+        var latitude = 1
+        var longitude = 2
+        var url = `http://www.google.com/maps?q=${latitude},${longitude}`
+
+        var message = generateLocationMessage(from, latitude, longitude)
+        expect(message).toInclude({url, from})
+        expect(message.createdAt).toBeA('number')
+
+
+
+    })
 })
